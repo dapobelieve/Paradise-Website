97149f0b5570f8adc4c2e0316613e7a8ce1c9723
dapobelieve <dredsn@gmail.com>
2018-09-19 08:37:23 +0100

    updates to home page

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -3,28 +3,25 @@
         <div class="container">
             <div class="footer-inner">
                 <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <div class="footer-content">
                             <div class="logo-footer"><img id="logo-footer" src="/assets/images/logo_light_blue.png" alt=""></div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Lorem ipsum dolor sit amet, illo vel dolorum soluta consectetur adipisicing... <a href="page-about.html">Learn More<i class="fa fa-long-arrow-right pl-5"></i></a></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Lorem ipsum dolor sit amet, illo vel dolorum soluta consectetur adipisicing... <a href="{{ route('site.about') }}">Learn More<i class="fa fa-long-arrow-right pl-5"></i></a></p>
                         </div>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <div class="footer-content">
                             <h2 class="title">Useful Links</h2>
                             <div class="separator-2"></div>
                             <nav>
                                 <ul class="nav nav-pills nav-stacked">
-                                    <li><a target="_blank" href="#">Support</a></li>
-                                    <li><a href="#">Privacy</a></li>
-                                    <li><a href="#">Terms</a></li>
-                                    <li><a href="page-about.html">About</a></li>
-                                    <li><a href="page-contact.html">Contact</a></li>
+                                    <li><a href="{{ route('site.store') }}">Store</a></li>
+                                    <li><a href="{{ route('site.contact') }}">Contact</a></li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <div class="footer-content">
                             <h2 class="title">Popular Tags</h2>
                             <div class="separator-2"></div>
@@ -47,39 +44,8 @@
                                 <div class="tag">
                                     <a href="#">culture</a>
                                 </div>
-                                <div class="tag">
-                                    <a href="#">health</a>
-                                </div>
-                                <div class="tag">
-                                    <a href="#">sports</a>
-                                </div>
-                                <div class="tag">
-                                    <a href="#">life style</a>
-                                </div>
-                                <div class="tag">
-                                    <a href="#">books</a>
-                                </div>
-                                <div class="tag">
-                                    <a href="#">lorem</a>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="footer-content">
-                            <h2 class="title">Contact</h2>
-                            <div class="separator-2"></div>
-                            <p>Lorem ipsum dolor sit amet accusamus excepturi sint dicta maxime?</p>
-                            <form>
-                                <div class="margin-clear form-group has-feedback">
-                                    <label class="sr-only" for="subscribe">Email address</label>
-                                    <input type="email" class="form-control" id="subscribe" placeholder="Enter email" name="subscribe" required="">
-                                    <i class="fa fa-envelope form-control-feedback"></i>
-                                </div>
-                                <div class="text-right">
-                                    <button type="submit" class="btn btn-default btn-sm btn-animated">Submit<i class="fa fa-send"></i></button>                                     
-                                </div>
-                            </form>                                     
                         </div>
                     </div>
                 </div>
