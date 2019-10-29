bb609c2b37b17b734918a1e0c2dc7728acfed7cf
Believe <believe@Believes-MacBook-Pro.local>
2019-10-29 21:21:21 +0100

    updates to migrations

--- database/migrations/2019_10_01_200405_create_records_table.php
+++ database/migrations/2019_10_01_200405_create_records_table.php
@@ -14,15 +14,15 @@ class CreateRecordsTable extends Migration
     public function up()
     {
         Schema::create('records', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('user_id');
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->text('service');
             $table->float('price', 8, 2);
             $table->string('name')->nullable();
             $table->string('email')->nullable();
             $table->string('phone', 11)->nullable();
             $table->enum('status', ['PAID', 'NOT PAID'])->default('NOT PAID');
-            $table->string('ref', 12)->unique();
+            $table->string('ref', 16)->unique();
             $table->softDeletes();
             $table->timestamp('paid_at')->nullable();
             $table->timestamps();
